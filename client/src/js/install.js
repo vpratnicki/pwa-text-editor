a57a008c8f279ecfc8295fb2334e1d89c0445903
vpratnicki <vanessa@pratnicki.net>
2022-12-04 15:59:14 -0500

    Chore: implemented install functionality.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,24 @@
-const butInstall = document.getElementById('buttonInstall');
+const buttonInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    buttonInstall.style.visibility = 'visible';
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+buttonInstall.addEventListener('click', async () => {
+    event.prompt();
+    buttonInstall.setAttribute('disabled', true);
+    buttonInstall.textContent = 'Installed!';
+  });
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('appinstalled', event);
+  });
+
+
+  
+
